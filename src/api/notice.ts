02d8acba7674d44a82a8789872dd96606711c2dd
Aashish John <aashishjohn25@gmail.com>
2024-04-01 23:40:36 -0400

    style: prefer default export for full file

--- src/api/notice.ts
+++ src/api/notice.ts
@@ -1,7 +1,7 @@
 import type { Notice, TitleContentNotice } from "@spoke/spoke-codegen";
 import type { GraphQLType } from "graphql";
 
-// eslint-disable-next-line import/prefer-default-export
+/* eslint-disable import/prefer-default-export */
 export function isTitleContentNotice(obj: Notice): obj is TitleContentNotice {
   return (
     (obj as TitleContentNotice & GraphQLType).__typename ===
