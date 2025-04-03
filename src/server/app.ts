e143be307118ab6022f3e3a1f0fafd145f5363a2
Aashish John <aashishjohn25@gmail.com>
2025-04-02 22:52:08 -0400

    fix: serve from webpack

--- src/server/app.ts
+++ src/server/app.ts
@@ -115,7 +115,6 @@ export const createApp = async () => {
   if (PUBLIC_DIR) {
     app.use(express.static(PUBLIC_DIR, { maxAge: "180 days" }));
   }
-  app.use("/", express.static("./public"));
 
   const graphqlRouter = await createGraphqlRouter();
 
