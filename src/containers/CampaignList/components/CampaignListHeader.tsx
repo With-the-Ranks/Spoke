9d4a47c6a089901c71f2a9741f257ca9ae5a1409
Sukhada Kulkarni <sukhada91@gmail.com>
2026-06-01 17:09:13 -0700

    feat(dialer): add campaign filtering to admin campaign list view

--- src/containers/CampaignList/components/CampaignListHeader.tsx
+++ src/containers/CampaignList/components/CampaignListHeader.tsx
@@ -1,5 +1,6 @@
-import { InputAdornment } from "@material-ui/core";
+import { InputAdornment, makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Chip from "@material-ui/core/Chip";
 import TextField from "@material-ui/core/TextField";
 import LibraryAddCheckOutlinedIcon from "@material-ui/icons/LibraryAddCheckOutlined";
 import OpenInNewOutlinedIcon from "@material-ui/icons/OpenInNewOutlined";
@@ -9,46 +10,100 @@ import { useDebouncedCallback } from "use-debounce";
 
 import type { CampaignDetailsForExport } from "../../../components/ExportMultipleCampaignDataDialog";
 
-const styles = {
+const useStyles = makeStyles({
   wrapper: {
     display: "flex",
+    flexDirection: "column",
+    paddingTop: 16
+  },
+  exportButton: {
+    alignSelf: "flex-start",
+    margin: "16px 0 8px"
+  },
+  filterRow: {
+    display: "flex",
     alignItems: "center",
-    justifyContent: "space-between",
-    paddingTop: "16px"
+    gap: 8
   }
-};
+});
 
 const DEBOUNCE_INTERVAL = 500;
 
+const FILTER_CHIPS = [
+  {
+    label: "Archived",
+    field: "isArchived",
+    activeValue: true,
+    inactiveValue: false
+  },
+  {
+    label: "Started",
+    field: "isStarted",
+    activeValue: true,
+    inactiveValue: undefined
+  },
+  {
+    label: "Not Started",
+    field: "isStarted",
+    activeValue: false,
+    inactiveValue: undefined
+  },
+  {
+    label: "Unsent Initials",
+    field: "hasUnsentInitialMessages",
+    activeValue: true,
+    inactiveValue: undefined
+  },
+  {
+    label: "Unhandled Replies",
+    field: "hasUnhandledMessages",
+    activeValue: true,
+    inactiveValue: undefined
+  },
+  {
+    label: "Unassigned Contacts",
+    field: "hasUnassignedContacts",
+    activeValue: true,
+    inactiveValue: undefined
+  }
+];
+
 interface Props {
   campaignDetailsForExport: CampaignDetailsForExport[];
+  campaignsFilter: Record<string, unknown>;
   filterByCampaignTitle: (str: string) => void;
+  onChipToggle: (
+    field: string,
+    activeValue: unknown,
+    inactiveValue: unknown
+  ) => void;
   onClick: () => void;
 }
 
 const CampaignListHeader: React.FC<Props> = ({
   campaignDetailsForExport,
+  campaignsFilter,
   onClick,
-  filterByCampaignTitle
+  filterByCampaignTitle,
+  onChipToggle
 }) => {
+  const classes = useStyles();
   const debounceSearchTerm = useDebouncedCallback((str: string) => {
     filterByCampaignTitle(str);
   }, DEBOUNCE_INTERVAL);
 
   const campaignIds = campaignDetailsForExport.map((campaign) => campaign.id);
-
   const isCampaignSelected = campaignIds.length > 0;
+
   return (
-    <div style={styles.wrapper}>
+    <div className={classes.wrapper}>
       <Button
         aria-controls="simple-menu"
         aria-haspopup="true"
         onClick={onClick}
         variant="outlined"
         color="primary"
-        style={{
-          margin: "16px 0px 16px 0px"
-        }}
+        className={classes.exportButton}
         startIcon={
           isCampaignSelected ? (
             <OpenInNewOutlinedIcon />
@@ -62,20 +117,35 @@ const CampaignListHeader: React.FC<Props> = ({
           ? `Export ${campaignIds.length} Campaign(s)`
           : "Select Campaign(s)"}
       </Button>
-      <TextField
-        id="outlined-basic"
-        label="Search..."
-        variant="outlined"
-        size="small"
-        InputProps={{
-          startAdornment: (
-            <InputAdornment position="start">
-              <SearchIcon />
-            </InputAdornment>
-          )
-        }}
-        onChange={(ev) => debounceSearchTerm(ev.target.value)}
-      />
+      <div className={classes.filterRow}>
+        {FILTER_CHIPS.map(({ label, field, activeValue, inactiveValue }) => {
+          const isActive = campaignsFilter[field] === activeValue;
+          return (
+            <Chip
+              key={label}
+              label={label}
+              clickable
+              color={isActive ? "primary" : "default"}
+              variant={isActive ? "default" : "outlined"}
+              onClick={() => onChipToggle(field, activeValue, inactiveValue)}
+            />
+          );
+        })}
+        <TextField
+          id="outlined-basic"
+          label="Search..."
+          variant="outlined"
+          size="small"
+          InputProps={{
+            startAdornment: (
+              <InputAdornment position="start">
+                <SearchIcon />
+              </InputAdornment>
+            )
+          }}
+          onChange={(ev) => debounceSearchTerm(ev.target.value)}
+        />
+      </div>
     </div>
   );
 };
