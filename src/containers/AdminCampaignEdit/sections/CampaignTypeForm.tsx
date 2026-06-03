d9cfd5e3ebb5b54a8730945e16299cc2bce17093
Sukhada Kulkarni <sukhada91@gmail.com>
2026-06-03 09:45:13 -0700

    feat(admin): add filters to campaign list view

---
+++ src/containers/AdminCampaignEdit/sections/CampaignTypeForm.tsx
@@ -0,0 +1,185 @@
+import type { ApolloQueryResult } from "@apollo/client";
+import { gql } from "@apollo/client";
+import Button from "@material-ui/core/Button";
+import FormControl from "@material-ui/core/FormControl";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import FormLabel from "@material-ui/core/FormLabel";
+import Radio from "@material-ui/core/Radio";
+import RadioGroup from "@material-ui/core/RadioGroup";
+import React from "react";
+import { compose } from "recompose";
+
+import { loadData } from "../../hoc/with-operations";
+import CampaignFormSectionHeading from "../components/CampaignFormSectionHeading";
+import type {
+  FullComponentProps,
+  RequiredComponentProps
+} from "../components/SectionWrapper";
+import { asSection } from "../components/SectionWrapper";
+
+type CampaignTypeValue = "SMS" | "CALL";
+
+interface CampaignTypeFormValues {
+  campaignType: CampaignTypeValue;
+}
+
+interface CampaignTypeHocProps {
+  data: {
+    campaign: CampaignTypeFormValues & { id: string; isStarted: boolean };
+  };
+  mutations: {
+    editCampaign(payload: CampaignTypeFormValues): ApolloQueryResult<any>;
+  };
+}
+
+interface CampaignTypeInnerProps
+  extends FullComponentProps,
+    CampaignTypeHocProps {}
+
+interface CampaignTypeState {
+  campaignType?: CampaignTypeValue;
+  isWorking: boolean;
+}
+
+class CampaignTypeForm extends React.Component<
+  CampaignTypeInnerProps,
+  CampaignTypeState
+> {
+  state: CampaignTypeState = {
+    campaignType: undefined,
+    isWorking: false
+  };
+
+  handleChange = (
+    _event: React.ChangeEvent<HTMLInputElement>,
+    value: string
+  ) => {
+    this.setState({ campaignType: value as CampaignTypeValue });
+  };
+
+  handleSubmit = async () => {
+    const { campaignType } = this.state;
+    const { editCampaign } = this.props.mutations;
+
+    this.setState({ isWorking: true });
+    try {
+      const response = await editCampaign({ campaignType: campaignType! });
+      if (response.errors) throw response.errors;
+      this.setState({ campaignType: undefined });
+    } catch (err) {
+      this.props.onError(err.message);
+    } finally {
+      this.setState({ isWorking: false });
+    }
+  };
+
+  render() {
+    const { isWorking } = this.state;
+    const {
+      data: { campaign },
+      saveLabel
+    } = this.props;
+
+    const campaignType =
+      this.state.campaignType !== undefined
+        ? this.state.campaignType
+        : campaign.campaignType;
+
+    const hasPendingChanges =
+      this.state.campaignType !== undefined &&
+      this.state.campaignType !== campaign.campaignType;
+
+    const isSaveDisabled =
+      isWorking || !hasPendingChanges || campaign.isStarted;
+    const finalSaveLabel = isWorking ? "Working..." : saveLabel;
+
+    return (
+      <div>
+        <CampaignFormSectionHeading
+          title="Campaign type"
+          subtitle="Choose whether this campaign will send SMS messages or make phone calls."
+        />
+        <FormControl component="fieldset" disabled={campaign.isStarted}>
+          <FormLabel component="legend">Type</FormLabel>
+          <RadioGroup
+            name="campaignType"
+            value={campaignType}
+            onChange={this.handleChange}
+            row
+          >
+            <FormControlLabel value="SMS" control={<Radio />} label="SMS" />
+            <FormControlLabel value="CALL" control={<Radio />} label="Call" />
+          </RadioGroup>
+        </FormControl>
+        {campaign.isStarted && (
+          <p style={{ color: "gray", fontSize: 12 }}>
+            Campaign type cannot be changed after the campaign has started.
+          </p>
+        )}
+        <Button
+          variant="contained"
+          disabled={isSaveDisabled}
+          onClick={this.handleSubmit}
+          style={{ marginTop: 16 }}
+        >
+          {finalSaveLabel}
+        </Button>
+      </div>
+    );
+  }
+}
+
+const queries = {
+  data: {
+    query: gql`
+      query getCampaignType($campaignId: String!) {
+        campaign(id: $campaignId) {
+          id
+          isStarted
+          campaignType
+        }
+      }
+    `,
+    options: (ownProps: CampaignTypeInnerProps) => ({
+      variables: {
+        campaignId: ownProps.campaignId
+      }
+    })
+  }
+};
+
+const mutations = {
+  editCampaign: (ownProps: CampaignTypeInnerProps) => (
+    payload: CampaignTypeFormValues
+  ) => ({
+    mutation: gql`
+      mutation editCampaignType(
+        $campaignId: String!
+        $payload: CampaignInput!
+      ) {
+        editCampaign(id: $campaignId, campaign: $payload) {
+          id
+          campaignType
+        }
+      }
+    `,
+    variables: {
+      campaignId: ownProps.campaignId,
+      payload
+    }
+  })
+};
+
+export default compose<CampaignTypeInnerProps, RequiredComponentProps>(
+  asSection({
+    title: "Campaign Type",
+    readinessName: "campaignType",
+    jobQueueNames: [],
+    expandAfterCampaignStarts: true,
+    expandableBySuperVolunteers: false
+  }),
+  loadData({
+    queries,
+    mutations
+  })
+)(CampaignTypeForm);
--- src/containers/CampaignList/components/CampaignListHeader.tsx
+++ src/containers/CampaignList/components/CampaignListHeader.tsx
@@ -1,11 +1,18 @@
 import { InputAdornment, makeStyles } from "@material-ui/core";
 import Button from "@material-ui/core/Button";
+import Checkbox from "@material-ui/core/Checkbox";
 import Chip from "@material-ui/core/Chip";
+import ListItemText from "@material-ui/core/ListItemText";
+import ListSubheader from "@material-ui/core/ListSubheader";
+import Menu from "@material-ui/core/Menu";
+import MenuItem from "@material-ui/core/MenuItem";
 import TextField from "@material-ui/core/TextField";
+import ArrowDropDownIcon from "@material-ui/icons/ArrowDropDown";
+import FilterListIcon from "@material-ui/icons/FilterList";
 import LibraryAddCheckOutlinedIcon from "@material-ui/icons/LibraryAddCheckOutlined";
 import OpenInNewOutlinedIcon from "@material-ui/icons/OpenInNewOutlined";
 import SearchIcon from "@material-ui/icons/Search";
-import React from "react";
+import React, { useState } from "react";
 import { useDebouncedCallback } from "use-debounce";
 
 import type { CampaignDetailsForExport } from "../../../components/ExportMultipleCampaignDataDialog";
@@ -23,48 +30,82 @@ const useStyles = makeStyles({
   filterRow: {
     display: "flex",
     alignItems: "center",
+    flexWrap: "wrap",
     gap: 8
+  },
+  filterButton: {
+    whiteSpace: "nowrap"
+  },
+  activeChips: {
+    display: "flex",
+    flexWrap: "wrap",
+    gap: 8
+  },
+  searchField: {
+    marginLeft: "auto"
   }
 });
 
 const DEBOUNCE_INTERVAL = 500;
 
-const FILTER_CHIPS = [
+interface FilterChip {
+  label: string;
+  field: string;
+  activeValue: unknown;
+  inactiveValue: unknown;
+}
+
+interface FilterGroup {
+  label: string;
+  chips: FilterChip[];
+}
+
+const FILTER_GROUPS: FilterGroup[] = [
   {
-    label: "Archived",
-    field: "isArchived",
-    activeValue: true,
-    inactiveValue: false
+    label: "Campaign status",
+    chips: [
+      {
+        label: "Archived",
+        field: "isArchived",
+        activeValue: true,
+        inactiveValue: false
+      },
+      {
+        label: "Started",
+        field: "isStarted",
+        activeValue: true,
+        inactiveValue: undefined
+      },
+      {
+        label: "Not Started",
+        field: "isStarted",
+        activeValue: false,
+        inactiveValue: undefined
+      }
+    ]
   },
   {
-    label: "Started",
-    field: "isStarted",
-    activeValue: true,
-    inactiveValue: undefined
-  },
-  {
-    label: "Not Started",
-    field: "isStarted",
-    activeValue: false,
-    inactiveValue: undefined
-  },
-  {
-    label: "Unsent Initials",
-    field: "hasUnsentInitialMessages",
-    activeValue: true,
-    inactiveValue: undefined
-  },
-  {
-    label: "Unhandled Replies",
-    field: "hasUnhandledMessages",
-    activeValue: true,
-    inactiveValue: undefined
-  },
-  {
-    label: "Unassigned Contacts",
-    field: "hasUnassignedContacts",
-    activeValue: true,
-    inactiveValue: undefined
+    label: "Message status",
+    chips: [
+      {
+        label: "Unsent Initials",
+        field: "hasUnsentInitialMessages",
+        activeValue: true,
+        inactiveValue: undefined
+      },
+      {
+        label: "Unhandled Replies",
+        field: "hasUnhandledMessages",
+        activeValue: true,
+        inactiveValue: undefined
+      },
+      {
+        label: "Unassigned Contacts",
+        field: "hasUnassignedContacts",
+        activeValue: true,
+        inactiveValue: undefined
+      }
+    ]
   }
 ];
 
@@ -88,12 +129,24 @@ const CampaignListHeader: React.FC<Props> = ({
   onChipToggle
 }) => {
   const classes = useStyles();
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
   const debounceSearchTerm = useDebouncedCallback((str: string) => {
     filterByCampaignTitle(str);
   }, DEBOUNCE_INTERVAL);
 
   const campaignIds = campaignDetailsForExport.map((campaign) => campaign.id);
   const isCampaignSelected = campaignIds.length > 0;
+
+  const isChipActive = (chip: FilterChip) =>
+    campaignsFilter[chip.field] === chip.activeValue;
+
+  const activeChips = FILTER_GROUPS.flatMap((group) => group.chips).filter(
+    isChipActive
+  );
+
+  const handleOpenMenu = (event: React.MouseEvent<HTMLButtonElement>) =>
+    setAnchorEl(event.currentTarget);
+  const handleCloseMenu = () => setAnchorEl(null);
 
   return (
     <div className={classes.wrapper}>
@@ -118,24 +171,62 @@ const CampaignListHeader: React.FC<Props> = ({
           : "Select Campaign(s)"}
       </Button>
       <div className={classes.filterRow}>
-        {FILTER_CHIPS.map(({ label, field, activeValue, inactiveValue }) => {
-          const isActive = campaignsFilter[field] === activeValue;
-          return (
+        <Button
+          aria-controls="campaign-filter-menu"
+          aria-haspopup="true"
+          variant="outlined"
+          className={classes.filterButton}
+          startIcon={<FilterListIcon />}
+          endIcon={<ArrowDropDownIcon />}
+          onClick={handleOpenMenu}
+        >
+          {activeChips.length > 0 ? `Filter (${activeChips.length})` : "Filter"}
+        </Button>
+        <Menu
+          id="campaign-filter-menu"
+          anchorEl={anchorEl}
+          keepMounted
+          open={Boolean(anchorEl)}
+          onClose={handleCloseMenu}
+          getContentAnchorEl={null}
+          anchorOrigin={{ vertical: "bottom", horizontal: "left" }}
+          transformOrigin={{ vertical: "top", horizontal: "left" }}
+        >
+          {FILTER_GROUPS.flatMap((group) => [
+            <ListSubheader key={`${group.label}-header`} disableSticky>
+              {group.label}
+            </ListSubheader>,
+            ...group.chips.map((chip) => (
+              <MenuItem
+                key={chip.label}
+                onClick={() =>
+                  onChipToggle(chip.field, chip.activeValue, chip.inactiveValue)
+                }
+              >
+                <Checkbox edge="start" checked={isChipActive(chip)} />
+                <ListItemText primary={chip.label} />
+              </MenuItem>
+            ))
+          ])}
+        </Menu>
+        <div className={classes.activeChips}>
+          {activeChips.map((chip) => (
             <Chip
-              key={label}
-              label={label}
-              clickable
-              color={isActive ? "primary" : "default"}
-              variant={isActive ? "default" : "outlined"}
-              onClick={() => onChipToggle(field, activeValue, inactiveValue)}
+              key={chip.label}
+              label={chip.label}
+              color="primary"
+              onDelete={() =>
+                onChipToggle(chip.field, chip.activeValue, chip.inactiveValue)
+              }
             />
-          );
-        })}
+          ))}
+        </div>
         <TextField
           id="outlined-basic"
           label="Search..."
           variant="outlined"
           size="small"
+          className={classes.searchField}
           InputProps={{
             startAdornment: (
               <InputAdornment position="start">
